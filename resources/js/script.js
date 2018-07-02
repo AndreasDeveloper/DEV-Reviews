c3873390b0a0528452503fee95181154f7dc581b
AndreasDEV <andreasmrdja16@gmail.com>
2018-07-02 21:15:04 +0200

    HTML-out the Reviews sub-section. Added animation to ABOUT US section. Multiple bug fixes

---
+++ resources/js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+
+/* SETUP FOR SMOOTH SCROLLING FOR ELEMETS */
+    $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  /* SETUP FOR SMOOTH SCROLLING FOR ELEMENTS */
+
+
+    /* ANIMATIONS ON SCROLL */
+
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated slideInLeft');
+    }, {
+         offset: '100%'
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
